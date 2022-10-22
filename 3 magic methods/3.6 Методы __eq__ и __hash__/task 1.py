1cfedb3100deb0175e317a9a7963f0e6340c103e
ilia <iliaman1993@gmail.com>
2022-10-22 10:50:56 +0300

    solved task 1 in module 3.6

---
+++ 3 magic methods/3.6 Методы __eq__ и __hash__/task 1.py
@@ -0,0 +1,9 @@
+class Rect:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __hash__(self):
+        return hash((self.width, self.height))
