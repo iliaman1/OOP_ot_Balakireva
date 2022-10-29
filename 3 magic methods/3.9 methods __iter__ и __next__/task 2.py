67f9d8374c1176addfa6524502a7eef65cfeaa32
ilia <iliaman1993@gmail.com>
2022-10-29 11:35:33 +0300

    solved task 2 in module 3.9

---
+++ 3 magic methods/3.9 methods __iter__ и __next__/task 2.py
@@ -0,0 +1,21 @@
+class TriangleListIterator:
+    def __init__(self, lst: list) -> list:
+        self.lst = lst
+
+    def __iter__(self):
+        for i in range(len(self.lst)):
+            for j in range(i + 1):
+                yield self.lst[i][j]
+
+
+lst = [['x00', 'x01', 'x02'],
+       ['x10', 'x11'],
+       ['x20', 'x21', 'x22', 'x23', 'x24'],
+       ['x30', 'x31', 'x32', 'x33']]
+
+it = TriangleListIterator(lst)
+for x in it:  # последовательный перебор всех элементов списка: x00, x10, x11, x20, ...
+    print(x)
+
+it_iter = iter(it)
+x = next(it_iter)
