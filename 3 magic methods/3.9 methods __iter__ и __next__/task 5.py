5616528d40f06c4fa1c77e387d6054c5323c891e
pupsik <iliaman1993@gmail.com>
2022-11-01 16:27:33 +0300

    continue to work on the task 5 in module 3.9

--- 3 magic methods/3.9 methods __iter__ и __next__/task 5.py
+++ 3 magic methods/3.9 methods __iter__ и __next__/task 5.py
@@ -2,16 +2,16 @@ from typing import Any
 
 
 class Cell:
-    def __init__(self, data: Any):
+    def __init__(self, data: Any = 0):
         self.__data = data
 
     @property
     def data(self) -> Any:
-        return self.data
+        return self.__data
 
     @data.setter
     def data(self, value: Any) -> None:
-        self.data = value
+        self.__data = value
 
 
 class TableValues:
@@ -21,7 +21,7 @@ class TableValues:
         self.type_data = type_data
         self.lst = [[Cell(0) for _ in range(self.cols)] for _ in range(self.rows)]
 
-    def __getitem__(self, item: tuple):
+    def __getitem__(self, item: tuple) -> Cell:
         if not isinstance(item, tuple) or 0 > item[0] > self.rows or 0 > item[1] > self.cols:
             raise IndexError('неверный индекс')
 
@@ -34,13 +34,30 @@ class TableValues:
         if not isinstance(value, int):
             raise TypeError('неверный тип присваиваемых данных')
 
-        self.lst[key[0]][key[1]] = value
+        self.lst[key[0]][key[1]].data = value
 
     def __iter__(self):
-        self.start_row = 0
-        self.start_coll = 0
+        self.current_row = 0
+        self.current_coll = 0
         return self
 
     def __next__(self):
-        if
+        if self.current_row < len(self.lst) and self.current_coll < len(self.lst[self.current_row]):
+            temp_res = self.lst[self.current_row]
+            self.current_row += 1
+            return temp_res
+
+        # elif self.current_coll >= len(self.lst[self.current_row])-1:
+        #     # temp_res = self.lst[self.current_row][self.current_coll]
+        #     self.current_coll = 0
+        #     self.current_row += 1
+        #     return self.lst[self.current_row][self.current_coll].data
+        else:
+            raise StopIteration
+
+
+test = TableValues(3, 3)
+for row in test:
+    for value in row:
+        print(value)
 
