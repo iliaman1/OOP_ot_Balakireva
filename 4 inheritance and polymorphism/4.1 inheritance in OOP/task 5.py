a2eeb2c57a0f337b67b254459844a6f416f8c9b9
pupsik <iliaman1993@gmail.com>
2022-11-04 19:13:33 +0300

    solved task 5 in module 4.1

---
+++ 4 inheritance and polymorphism/4.1 inheritance in OOP/task 5.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+
+class Validator:
+    def is_valid(self, data: Any) -> bool:
+        return True
+
+    def __call__(self, data: Any, *args, **kwargs) -> bool:
+        return self.is_valid(data)
+
+
+class IntegerValidator(Validator):
+    def __init__(self, min_value: int, max_value: int):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def is_valid(self, data: Any) -> bool:
+        if type(data) == int and self.max_value >= data >= self.min_value:
+            return True
+        raise ValueError('данные не прошли валидацию')
+
+
+class FloatValidator(Validator):
+    def __init__(self, min_value: int, max_value: int):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def is_valid(self, data: Any) -> bool:
+        if type(data) == int and self.max_value >= data >= self.min_value:
+            return True
+        raise ValueError('данные не прошли валидацию')
