279f7cc29c00760b3adea6a7f7e31e468027a7e4
pupsik <iliaman1993@gmail.com>
2022-11-08 19:51:03 +0300

    solved task 5 in module 4.3

---
+++ 4 inheritance and polymorphism/4.3 Inheritance. super() function and delegation/task 5.py
@@ -0,0 +1,32 @@
+def integer_params_decorated(method):
+    def wrapper(*args):
+        if all((type(i) == int for i in args[1:])):
+            return method(*args)
+
+        raise TypeError("аргументы должны быть целыми числами")
+
+    return wrapper
+
+
+def integer_params(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
+
+
+@integer_params
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
+
+    def __setitem__(self, key, value):
+        self.__coords[key] = value
+
+    def set_coords(self, *coords, reverse=False):
+        c = list(coords)
+        self.__coords = c if not reverse else c[::-1]
