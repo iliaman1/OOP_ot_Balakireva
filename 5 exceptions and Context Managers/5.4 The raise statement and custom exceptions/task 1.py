d37112b6dfd0763a203eb20c15468cbfccaa386d
ilia <iliaman1993@gmail.com>
2022-11-19 09:50:33 +0300

    solved task 1 in module 5.4.

---
+++ 5 exceptions and Context Managers/5.4 The raise statement and custom exceptions/task 1.py
@@ -0,0 +1,22 @@
+# здесь объявляйте классы
+class StringException(Exception):
+    pass
+
+
+class NegativeLengthString(StringException):
+    pass
+
+
+class ExceedLengthString(StringException):
+    pass
+
+
+try:
+    # здесь команда для генерации исключения
+    raise ExceedLengthString
+except NegativeLengthString:
+    print("NegativeLengthString")
+except ExceedLengthString:
+    print("ExceedLengthString")
+except StringException:
+    print("StringException")
